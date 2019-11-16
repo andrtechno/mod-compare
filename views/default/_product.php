ed63a1f3b7ea2b5f03e3de7f2839986e5a6256af
Andrew <andrew.panix@gmail.com>
2019-11-16 16:27:48 +0200

    init

--- views/default/_product.php
+++ views/default/_product.php
@@ -68,8 +68,12 @@ use panix\engine\Html;
 
             }
 
+            ?>
+        </div>
+        <div class="text-center">
+            <?php
             echo Html::a(Yii::t('app', 'DELETE'), ['/compare/default/remove', 'id' => $data->id], [
-                'class' => 'btn btn-link text-danger remove',
+                'class' => 'btn2 btn-link2 text-danger remove',
             ]);
             ?>
         </div>
